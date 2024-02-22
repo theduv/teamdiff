381250f78498821e503a4293963b9b9ea58915e2
theo <theo@theos-MacBook-Pro-2.local>
2024-02-22 13:33:02 +0100

    feat: can now press enter to submit the summoner field

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, memo, useState } from "react";
+import { ChangeEvent, KeyboardEvent, memo, useState } from "react";
 import { MdArrowForwardIos } from "react-icons/md";
 import { navigate } from "wouter/use-browser-location";
 
@@ -7,6 +7,12 @@ const HomeViewBase = () => {
 
   const onChangeSummonerValue = (e: ChangeEvent<HTMLInputElement>) => {
     setSummonerValue(e.target.value);
+  };
+
+  const inputKeydownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onClickSearch();
+    }
   };
 
   const onClickSearch = () => {
@@ -26,6 +32,7 @@ const HomeViewBase = () => {
           <input
             value={summonerValue}
             onChange={onChangeSummonerValue}
+            onKeyDown={inputKeydownHandler}
             className="bg-gray-200 rounded-md px-4 py-2"
             placeholder="AlphaDraven#EUW"
           />
