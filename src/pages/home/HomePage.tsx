74ae71b374d93032130dcee7687d552d92ae3ea0
theduv <tduval@student.42.fr>
2024-02-25 19:20:24 +0100

    feat: added error under input for invalid summoner names

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -6,8 +6,12 @@ import { isSummonerNameAndTagValid } from "../../lib/functions/isSummonerNameAnd
 
 const HomePageBase = () => {
   const [summonerValue, setSummonerValue] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const onChangeSummonerValue = (e: ChangeEvent<HTMLInputElement>) => {
+    if (error) {
+      setError(null);
+    }
     setSummonerValue(e.target.value);
   };
 
@@ -18,7 +22,10 @@ const HomePageBase = () => {
   };
 
   const onClickSearch = () => {
-    if (!isSummonerNameAndTagValid(summonerValue, "#")) return;
+    if (!isSummonerNameAndTagValid(summonerValue, "#")) {
+      setError("Invalid summoner name format.");
+      return;
+    }
     const [summonerName, summonerTag] = summonerValue.split("#");
 
     navigate(`/summoner/${summonerName}-${summonerTag}`);
@@ -26,19 +33,22 @@ const HomePageBase = () => {
 
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="flex flex-col space-y-4 items-center">
+      <div className="flex flex-col space-y-4">
         <h1 className="text-gray-100 text-4xl">Summoner name</h1>
         <div className="flex items-center space-x-4">
           <input
             value={summonerValue}
             onChange={onChangeSummonerValue}
             onKeyDown={inputKeydownHandler}
-            className="bg-gray-200 rounded-md px-4 py-2"
+            className="bg-gray-200 rounded-md px-4 py-2 outline-none"
             placeholder="AlphaDraven#EUW"
           />
           <button onClick={onClickSearch}>
             <MdArrowForwardIos color="white" size={32} />
           </button>
+        </div>
+        <div className="h-10">
+          {error && <span className="text-red-400">{error}</span>}
         </div>
       </div>
     </div>
