4e33b504b95245b8e9c07e1db15dae9b6c9c6f6a
Théo <tduval@student.42.fr>
2024-03-03 12:51:33 +0100

    feat: left border on review

--- src/pages/summoner-v3/components/recent-reviews/Review.tsx
+++ src/pages/summoner-v3/components/recent-reviews/Review.tsx
@@ -25,38 +25,45 @@ const ReviewBase = ({ review }: ReviewProps) => {
   if (!summoner || !riotSummoner) return null;
 
   return (
-    <div
-      className={clsx("flex items-center space-x-4 p-4 rounded-lg w-full", {
-        "bg-red-100": !review.hasWon,
-        "bg-green-100": review.hasWon,
-      })}
-    >
-      <img
-        src={getChampionIconURL(review.champion.id)}
-        className="rounded-full"
-        width={CHAMPION_ICON_SIZE}
-        height={CHAMPION_ICON_SIZE}
-      />
-      <div className="flex flex-col">
-        <Link
-          href={`/summoner/${summoner.name}-${summoner.tag}`}
-          className="flex space-x-2"
-        >
+    <div className="flex">
+      <div
+        className={clsx("flex rounded-lg w-full", {
+          "bg-red-100": !review.hasWon,
+          "bg-green-100": review.hasWon,
+        })}
+      >
+        <div
+          className={clsx("h-full w-3 rounded-l-lg", {
+            "bg-red-300": !review.hasWon,
+            "bg-green-300": review.hasWon,
+          })}
+        />
+        <div className="flex items-center space-x-4 p-4 w-full">
           <img
-            src={getSummonerIconURL(riotSummoner.profileIconId)}
-            width={SUMMONER_ICON_SIZE}
-            height={SUMMONER_ICON_SIZE}
-            className="rounded-full max-w-fit"
+            src={getChampionIconURL(review.champion.id)}
+            className="rounded-full"
+            width={CHAMPION_ICON_SIZE}
+            height={CHAMPION_ICON_SIZE}
           />
-          <h2 className="font-bold text-lg">
-            {summoner.name}#{summoner.tag}
-          </h2>
-        </Link>
-        <div className="flex items-center space-x-2">
-          <StarRating rating={review.grade} size={"xsmall"} />
-        </div>
-        <div className="flex items-center justify-between space-x-2">
-          <span>{review.comment}</span>
+          <div className="flex flex-col">
+            <div className="flex items-center space-x-2">
+              <img
+                src={getSummonerIconURL(riotSummoner.profileIconId)}
+                width={SUMMONER_ICON_SIZE}
+                height={SUMMONER_ICON_SIZE}
+                className="rounded-full max-w-fit"
+              />
+              <StarRating rating={review.grade} size={"xsmall"} />
+            </div>
+            <Link href={`/summoner/${summoner.name}-${summoner.tag}`}>
+              <h2 className="font-bold text-xl">
+                {summoner.name}#{summoner.tag}
+              </h2>
+            </Link>
+            <div className="flex items-center justify-between space-x-2">
+              <span>{review.comment}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
